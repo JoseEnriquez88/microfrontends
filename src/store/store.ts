e9eeff80ace9f5488938392ead9ad3c605ad3091
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-08 12:16:34 -0300

    update redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import charactersReducer from "./features/charactersSlice";
+import favoritesReducer from "./features/favoritesSlice";
+import { combineReducers } from "redux";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart"],
+  whitelist: ["favorites"],
 };
 
+const rootReducer = combineReducers({
+  characters: charactersReducer,
+  favorites: favoritesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
@@ -17,5 +27,8 @@ const store = configureStore({
       },
     }),
 });
+
 export const persistor = persistStore(store);
 export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
