09fb77e26c436f5a038cdec99fe0638dd3d1e7f7
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-11 17:03:42 -0300

    update

---
+++ swiper.d.ts
@@ -0,0 +1,29 @@
+declare module "swiper/css" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "swiper/css/navigation" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "swiper/css/pagination" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "swiper/css/scrollbar" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "swiper/css/effect-fade" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: Record<string, string>;
+  export default content;
+}
