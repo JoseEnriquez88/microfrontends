e26d60c87837555b487cda9b2afef0bddd2d1310
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-11 17:03:27 -0300

    added hero motion

---
+++ src/utils/motion/hero.ts
@@ -0,0 +1,44 @@
+export const varianstImage = {
+  hidden: { opacity: 0, y: -20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6 },
+  },
+};
+
+export const variantsH1 = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.2,
+    },
+  },
+};
+
+export const variantsH3 = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.4,
+    },
+  },
+};
+
+export const variantsContainer = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.6,
+    },
+  },
+};
--- src/views/Hero/Hero.tsx
+++ src/views/Hero/Hero.tsx
@@ -1,26 +1,101 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+// motionVariants.ts
+export const varianstImage = {
+  hidden: { opacity: 0, y: -20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6 },
+  },
+};
+
+// motionVariants.ts
+export const variantsH1 = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.2,
+    },
+  },
+};
+
+// motionVariants.ts
+export const variantsH3 = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.4,
+    },
+  },
+};
+
+// motionVariants.ts
+export const variantsContainer = {
+  hidden: { opacity: 0, y: 10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: 0.6,
+    },
+  },
+};
+
+
 const Hero = () => {
   return (
     <div className="relative font-rm-mont bg-hero h-screen w-screen bg-[url('/assets/images/hero.png')] bg-cover bg-center pb-12 md:px-50 flex flex-col items-center justify-center md:pb-20 gap-[20px] md:gap-10">
-      <img
+      <motion.img
         src="/assets/images/logo.png"
         alt="logo"
+        initial="hidden"
+        animate="visible"
+        variants={varianstImage}
         className="relative w-[343px] h-[121px] md:w-[508px] md:h-auto"
       />
-      <h1 className="relative w-[343px] md:w-auto font-bold text-rm-neutral-50 text-center text-[24px] lg:text-5xl leading-none tracking-normal drop-shadow-md brightness-125">
+
+      <motion.h1
+        initial="hidden"
+        animate="visible"
+        variants={variantsH1}
+        className="relative w-[343px] md:w-auto font-bold text-rm-neutral-50 text-center text-[24px] lg:text-5xl leading-none tracking-normal drop-shadow-md brightness-125"
+      >
         Bienvenido a Rick and Morty
-      </h1>
-      <h3 className="relative font-rm-mont w-[343px] md:w-[872px] font-semibold text-neutral-50 md:font-medium text-[14px] leading-[20px] tracking-normal  md:text-[18px]  text-center md:leading-8  drop-shadow-md brightness-125">
+      </motion.h1>
+
+      <motion.h3
+        initial="hidden"
+        animate="visible"
+        variants={variantsH3}
+        className="relative font-rm-mont w-[343px] md:w-[872px] font-semibold text-neutral-50 md:font-medium text-[14px] leading-[20px] tracking-normal md:text-[18px] text-center md:leading-8 drop-shadow-md brightness-125"
+      >
         En esta prueba, evaluaremos su capacidad para construir la aplicación
         mediante el análisis de código y la reproducción del siguiente diseño.
-      </h3>
-      <Link
-        to="/home"
-        className="relative flex items-center justify-center rounded-full bg-rm-primary-500 hover:bg-rm-primary-300 w-[119px] h-[44px] md:w-[119px] md:h-[44px] font-semibold text-[14px] md:text-lg leading-8 tracking-normal text-center transition-colors duration-300 ease-in-out drop-shadow-md brightness-125"
+      </motion.h3>
+
+      <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variantsContainer}
       >
-        Comenzar
-      </Link>
+        <Link
+          to="/home"
+          className="relative flex items-center justify-center rounded-full bg-rm-primary-500 hover:bg-rm-primary-300 w-[119px] h-[44px] md:w-[119px] md:h-[44px] font-semibold text-[14px] md:text-lg leading-8 tracking-normal text-center transition-colors duration-300 ease-in-out drop-shadow-md brightness-125"
+        >
+          Comenzar
+        </Link>
+      </motion.div>
     </div>
   );
 };
+
 export default Hero;
