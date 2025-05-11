f4dc4c11f98d5eccf365e8de7aa37145a01e72b3
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-05-11 17:27:47 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
+import Loader from "./components/Loader/Loader";
 
 const Hero = lazy(() => import("./views/Hero/Hero"));
 const Home = lazy(() => import("./views/Home/Home"));
-const CharacterDetail = lazy(() => import("./views/CharacterDetail/CharacterDetail"));
+const CharacterDetail = lazy(
+  () => import("./views/CharacterDetail/CharacterDetail")
+);
 
 function App() {
   const location = useLocation();
   const state = location.state as { backgroundLocation?: Location };
   const backgroundLocation = state?.backgroundLocation;
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <Routes location={backgroundLocation || location}>
-        <Route path="/" element={<Hero />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/character/:id" element={<Home />} />
-      </Routes>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Routes location={backgroundLocation || location}>
+            <Route path="/" element={<Hero />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/character/:id" element={<Home />} />
+          </Routes>
 
-      {backgroundLocation && (
-        <Routes>
-          <Route
-            path="/character/:id"
-            element={
-              <Suspense fallback={null}>
-                <CharacterDetail />
-              </Suspense>
-            }
-          />
-        </Routes>
+          {backgroundLocation && (
+            <Routes>
+              <Route
+                path="/character/:id"
+                element={
+                  <Suspense fallback={null}>
+                    <CharacterDetail />
+                  </Suspense>
+                }
+              />
+            </Routes>
+          )}
+        </>
       )}
     </>
   );
